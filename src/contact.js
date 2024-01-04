93618d4a543e1c69601e59164978cb114e630498
Ed Benedict Quia <edbenedictquia@gmail.com>
2024-01-04 20:52:38 +0800

    finish contact page

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,31 @@
 export default function contactPage() {
     const main = document.getElementById('main');
-    const home = document.getElementById('home');
-    const menu = document.getElementById('menu');
+    const h1 = document.createElement('h1');
+    const div = document.createElement('div');
+    // const p = document.createElement('p');
 
-    // main[0].innerHTML = "asd";
-    // console.log(home);
-    // home.disabled = false;
-    main.innerHTML = "THIS IS CONTACT";
+    main.textContent = "";
 
+    h1.innerHTML = "Contact Us";
+
+    // p.textContent = "The Owner +63912345678 theownersemail@gmail.com"
+
+
+    let p1 = createParagraph("The Owner");
+    let p2 = createParagraph("+63912345678");
+    let p3 = createParagraph("theownersemail@gmail.com");
+
+    main.appendChild(h1);
+    main.appendChild(div);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
     return main;
+  }
+
+  function createParagraph(text) {
+    const p = document.createElement('p');
+    p.innerHTML = text;
+
+    return p;
   }
